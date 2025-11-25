38ee8f23eab1d0ea8025854fe2a3d3ddb2d0c5e2
Sa1il-Patel <salil.patel_cs23@gla.ac.in>
2025-11-25 18:59:19 +0530

    variables
    Added new variables for AWS region, application name, GitHub details, and OAuth token.

--- aws_devops_repo/infrastructure/variables.tf
+++ aws_devops_repo/infrastructure/variables.tf
@@ -1,1 +1,6 @@
-variable "region" { default="us-east-1" }
+variable "aws_region" { default = "us-east-1" }
+variable "app_name" { default = "my-web-app" }
+variable "github_owner" { description = "GitHub owner/org" }
+variable "github_repo" { description = "repo name" }
+variable "github_branch" { default = "main" }
+variable "github_oauth_token" { description = "OAuth token for GitHub" }
